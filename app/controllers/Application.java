51be049b53fb8b923599400d598904adf6e357e0
enwoo <enwoo@enwoo-GA-A75M-DS2>
2013-01-21 21:04:28 +0100

    Change-Id: I8043e8f48eb03d4badcfac701fec5c0581d1061a Signed-off-by: enwoo <enwoo@enwoo-GA-A75M-DS2>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,6 +14,7 @@ public class Application extends Controller {
     	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
     	render(frontPost,olderPosts);
     	//test
+    	
     }
 
 }
