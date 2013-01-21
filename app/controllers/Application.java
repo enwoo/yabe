0759067e7bb4ab77d535358567bbf364971baa68
enwoo <lducamp@gmail.com>
2013-01-21 21:02:25 +0100

    Change-Id: I087a8c8700b2b6c2c961ad4dc9b82755d61ac244 Signed-off-by: enwoo <enwoo@enwoo-GA-A75M-DS2>

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,19 @@
 package controllers;
 
-import models.User;
+import java.util.List;
+
+import models.Post;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
-    	User u = new User("bob@gmail.com","secret","boby",false);
-        renderJSON(u);
+//    	User u = new User("bob@gmail.com","secret","boby",false);
+//        renderJSON(u);
+    	Post frontPost = Post.find("order by postedAt desc").first();
+    	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+    	render(frontPost,olderPosts);
+    	//test
     }
 
 }
